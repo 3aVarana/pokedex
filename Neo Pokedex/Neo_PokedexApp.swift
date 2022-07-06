73d5bae6d905d40b6afa679a56c30026f1b7ad0a
Andres Arana <3av.arana@gmail.com>
2022-07-05 22:53:32 -0400

    Create base project

---
+++ Neo Pokedex/Neo_PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neo_PokedexApp.swift
+//  Neo Pokedex
+//
+//  Created by Victor Arana on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Neo_PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
